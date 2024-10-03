7c9b299ebc5cad6743e4b886186d36a635177178
Felipe Vaz Peres <felipe.vzps@gmail.com>
2024-10-03 10:54:19 -0300

    describe functions with docstring

--- load.py
+++ load.py
@@ -1,20 +1,35 @@
-import numpy as np
 import struct
+import numpy as np
 
-# carregar o imagens no formato ubyte 
 def load_mnist_images(file_path):
+    """
+    Loads MNIST images from an ubyte format file.
+
+    Args:
+        file_path (str): Path to the ubyte format image file.
+
+    Returns:
+        np.ndarray: A NumPy array containing the MNIST images. The array shape is
+                    (n_images, n_pixels), where each image is flattened into a
+                    vector of size (rows * cols).
+    """
     with open(file_path, 'rb') as f:
-        # ignorar o cabeçalho (16 bytes)
-        magic, num_images, rows, cols = struct.unpack(">IIII", f.read(16))
-        # lendo o resto do arquivo (cada imagem é de tamanho rows x cols)
-        images = np.frombuffer(f.read(), dtype=np.uint8).reshape(num_images, rows * cols)
+        magic, num_images, rows, cols = struct.unpack(">IIII", f.read(16))                       # ignore the header (16 bytes)
+        images = np.frombuffer(f.read(), dtype=np.uint8).reshape(num_images, rows * cols)        # read the rest of the file (each image is rows x cols in size)
     return images
 
-# carregar os rótulos no formato ubyte 
 def load_mnist_labels(file_path):
+    """
+    Loads MNIST labels from an ubyte format file.
+
+    Args:
+        file_path (str): Path to the ubyte format label file.
+
+    Returns:
+        np.ndarray: A NumPy array containing the MNIST labels.
+    """
     with open(file_path, 'rb') as f:
-        # ignorar o cabeçalho (8 bytes)
-        magic, num_labels = struct.unpack(">II", f.read(8))
-        # lendo o resto do arquivo (rótulos são uint8)
-        labels = np.frombuffer(f.read(), dtype=np.uint8)
+        
+        magic, num_labels = struct.unpack(">II", f.read(8))                                      # ignore the header (8 bytes)
+        labels = np.frombuffer(f.read(), dtype=np.uint8)                                         # read the rest of the file (labels are stored as uint8)
     return labels
